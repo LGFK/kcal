794345420a6360cd1ac029c9f1d694cd1b0fdf9e
hamlet <107580695+HamletArut@users.noreply.github.com>
2024-03-05 18:58:07 +0200

    createAdditionalInfoForUser

--- Project/Project/Controllers/UserController.cs
+++ Project/Project/Controllers/UserController.cs
@@ -14,14 +14,79 @@ namespace Project.Controllers
         
         UserManager<IdentityUser> _userManager;
         private readonly SettingsRepo _sR;
-        
+        UserViewModel _userView;
+
 
         public UserController(UserManager<IdentityUser> userManager, SettingsRepo Sr)
         {
             _userManager = userManager;
             _sR = Sr;
+            //CreateUserViewModel();
+            _userView = new UserViewModel();
         }
+
+        private async void CreateUserViewModel()
+        {
+            //var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            //var settings = await _sR.GetSettings(user.Id);
+            //if (settings == null)
+            //{
+            //    var settingsToAdd = new Settings()
+            //    {
+            //        EmailNotifications = false,
+            //        GoalId = 1,
+            //        Theme = true,
+            //        UserId = user.Id
+            //    };
+
+            //    var uDescriptionToAdd = new UserDescription()
+            //    {
+            //        Age = 0,
+            //        WeightKG = 0,
+            //        GenderId = 1,
+            //        HeightCM = 0,
+            //        UserId = user.Id
+            //    };
+            //    await _sR.AddSettings(settingsToAdd, uDescriptionToAdd);
+            //    settings = await _sR.GetSettings(user.Id) ?? new Settings();
+            //}
+            //var uD = await _sR.GetUserDescription(user.Id) ?? new UserDescription();
+            //_userView = await UserInfoContainerFactory.GetUserInfoContainer(user, settings, uD);
+        }
+
+        [HttpGet]
         public async Task<IActionResult> Index()
+        {
+            //var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            //var settings = await _sR.GetSettings(user.Id);
+            //if (settings == null)
+            //{
+            //    var settingsToAdd = new Settings()
+            //    {
+            //        EmailNotifications = false,
+            //        GoalId = 1,
+            //        Theme = true,
+            //        UserId = user.Id
+            //    };
+
+            //    var uDescriptionToAdd = new UserDescription()
+            //    {
+            //        Age = 0,
+            //        WeightKG = 0,
+            //        GenderId = 1,
+            //        HeightCM = 0,
+            //        UserId = user.Id
+            //    };
+            //    await _sR.AddSettings(settingsToAdd,uDescriptionToAdd);
+            //    settings = await _sR.GetSettings(user.Id);
+            //}
+            //var uD =await _sR.GetUserDescription(user.Id);
+            //UserViewModel  _userInfoContainer = await UserInfoContainerFactory.GetUserInfoContainer(user, settings, uD);//это модель которую надо использовать во view;
+            
+            return View("AdditionalInfo");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserViewModel newUserView)
         {
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
             var settings = await _sR.GetSettings(user.Id);
@@ -34,7 +99,7 @@ namespace Project.Controllers
                     Theme = true,
                     UserId = user.Id
                 };
-                
+
                 var uDescriptionToAdd = new UserDescription()
                 {
                     Age = 0,
@@ -43,12 +108,19 @@ namespace Project.Controllers
                     HeightCM = 0,
                     UserId = user.Id
                 };
-                await _sR.AddSettings(settingsToAdd,uDescriptionToAdd);
-                settings = await _sR.GetSettings(user.Id);
+                await _sR.AddSettings(settingsToAdd, uDescriptionToAdd);
+                settings = await _sR.GetSettings(user.Id) ?? new Settings();
             }
-            var uD =await _sR.GetUserDescription(user.Id);
-            UserViewModel  _userInfoContainer = await UserInfoContainerFactory.GetUserInfoContainer(user, settings, uD);//это модель которую надо использовать во view;
-            return View();
+            var uD = await _sR.GetUserDescription(user.Id) ?? new UserDescription();
+            _userView = await UserInfoContainerFactory.GetUserInfoContainer(user, settings, uD);
+            _userView.Settings.GoalId = newUserView.Settings.GoalId;
+            _userView.UserDescription.HeightCM = newUserView.UserDescription.HeightCM;
+            _userView.UserDescription.WeightKG = newUserView.UserDescription.WeightKG;
+            _userView.UserDescription.Age = newUserView.UserDescription.Age;
+            _userView.UserDescription.GenderId = newUserView.UserDescription.GenderId;
+            // save in database
+
+            return RedirectToAction();
         }
     }
 }
